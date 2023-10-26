1cd1e8bc114b1c20a82db9d1641050df53a8e0da
Jean-Charles <randomboolean@gmail.com>
2023-10-26 17:22:44 +0200

    fix fusefetch

--- src/pywarp/fusefetch.py
+++ src/pywarp/fusefetch.py
@@ -19,6 +19,8 @@ import pandas
 
 from .splitfetch import sfetch
 
+from .fetch import _streamFetchResponse, _parseLine
+
 TIMESTAMP = 'ts'
 LATITUDE = 'lat'
 LONGITUDE = 'lon'
@@ -28,33 +30,59 @@ CLASSNAME = 'warp10.classname'
 LABELS = 'warp10.labels'
 ATTRIBUTES = 'warp10.attributes'
 
-def _expandMetadata(df):
-  for key in df.attrs[LABELS]:
-    df['l:' + key] = [df.attrs[LABELS][key]] * len(df)
-  
-  for key in df.attrs[ATTRIBUTES]:
-    df['a:' + key] = [df.attrs[ATTRIBUTES][key]] * len(df)
-
-  df.rename(columns={LATITUDE: LATITUDE + ':' + df.attrs[CLASSNAME],
-                     LONGITUDE: LONGITUDE + ':' + df.attrs[CLASSNAME],
-                     ELEVATION: ELEVATION + ':' + df.attrs[CLASSNAME]},
-            inplace=True)
-  
-  return df
-
-def ffetch(endpoint, token, selector, end, timespan):
+def ffetch(endpoint, token, selector, end, timespan, indexedByTimestamp=False):
   """
 Read data from a Warp 10 instance using the specified /api/v0/fetch endpoint.
 Outputs a single amalgamated pandas dataframe.
   """
 
-  dfs = sfetch(endpoint, token, selector, end, timespan, indexedByTimestamp=True)
+  headers = {}
+  headers['X-Warp10-Token'] = token
+  params = {}
+  params['selector'] = selector
+  params['end'] = str(end)
+  params['timespan'] = str(timespan)
+  params['showattr'] = 'true'
+  params['format'] = 'text'
 
-  if 0 == len(dfs):
-    return pandas.DataFrame()
+  #
+  # We will create the dataframe using from_records method
+  # data is passed as a list of dict. Each dict represents a row.
+  # for each new data point from the stream, we need to check if there is a row where it can be inserted (adding only one col)
+  # it must have the same ts, label set and attribute set to be inserted in this row
+  # if not we add a new row
+  #
 
-  fdf = _expandMetadata(dfs[0])
-  for df in dfs[1:]:
-    fdf = fdf.combine_first(_expandMetadata(df))
+  datadict = {} # key is an ident made of ts/labels/attributes, and value is the row dict
+  for line in _streamFetchResponse(endpoint, headers, params):
+    (ts,lat,lon,elev,cls,lbls,attributes,value) = _parseLine(line)
+    if not cls:
+      cls = lastcls
+      lbls = lastlbls
+      attributes = lastattr
+    else:
+      lastcls = cls
+      lastlbls = lbls
+      lastattr = attributes
 
-  return fdf
+    ident = str(ts) + str(lbls) + str(attributes)
+    if not(ident in datadict):
+      datadict[ident] = {TIMESTAMP: ts}
+      for k in lbls:
+        datadict[ident]['l:' + k] = lbls[k]
+      for k in attributes:
+        datadict[ident]['a:' + k] = attributes[k]
+    
+    datadict[ident][VALUE + ':' + cls] = value
+    if lat is not None:
+      datadict[ident][LATITUDE + ':' + cls] = lat
+      datadict[ident][LONGITUDE + ':' + cls] = lon
+    if elev is not None:
+      datadict[ident][ELEVATION + ':' + cls] = elev
+
+  df = pandas.DataFrame.from_records(iter(datadict.values()))
+
+  if indexedByTimestamp:
+    df = df.set_index(TIMESTAMP)
+  
+  return df
